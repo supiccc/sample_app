28d146b8db4457e092732e95887fed556a9c1af9
supiccc <supiccc@gmail.com>
2017-02-21 13:37:48 +0800

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
 
   get '/contact', to: 'static_pages#contact'
 
+  post '/signup', to: 'users#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+
+  resources :users
 end
