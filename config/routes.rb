05a36dcc7f71ddd46e43fdb9114245132634e399
supiccc <supiccc@gmail.com>
2017-02-22 15:39:30 +0800

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   get '/signup', to: 'users#new'
 
   get '/help', to: 'static_pages#help'
@@ -9,6 +11,12 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
 
   post '/signup', to: 'users#create'
+ 
+  get '/login', to: 'sessions#new'
+
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -25,6 +25,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   	end
   	follow_redirect!
   	assert_template 'users/show'
-  	assert_not flash.blank?
+  	assert_not flash.empty?
+    assert is_logged_in?
   end
 end
