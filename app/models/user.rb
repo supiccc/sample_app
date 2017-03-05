b03ccb43275cfac6d586be1cca6509c0ce465e24
supiccc <supiccc@gmail.com>
2017-03-05 21:08:55 +0800

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-	attr_accessor :remember_token, :activation_token
+	attr_accessor :remember_token, :activation_token, :reset_token
 
 	before_save :downcase_email
 	before_create :create_activation_digest
@@ -48,6 +48,22 @@ class User < ApplicationRecord
  		UserMailer.account_activation(self).deliver_now
  	end
 
+    def create_reset_digest
+ 		self.reset_token = User.new_token
+ 		update_attribute(:reset_digest, User.digest(reset_token))
+ 		update_attribute(:reset_sent_at, Time.zone.now)
+ 		# self.update_columns(reset_digest: true, reset_sent_at: Time.zone.now)
+ 	end
+
+ 	def send_password_reset_email
+ 		UserMailer.password_reset(self).deliver_now
+ 	end
+
+ 	def password_reset_expired?
+ 		reset_sent_at < 2.hours.ago
+ 	end
+
+
  	private
  		def create_activation_digest
  			self.activation_token = User.new_token
@@ -57,5 +73,6 @@ class User < ApplicationRecord
  		def downcase_email
  			self.email = email.downcase
  		end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get '/signup', to: 'users#new'
 
   get '/help', to: 'static_pages#help'
@@ -21,4 +25,5 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:edit, :new, :create, :update]
 end
