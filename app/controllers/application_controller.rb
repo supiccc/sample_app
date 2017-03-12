05e6599fb1e6d6e8aa8c3fa82d6c7fd9de025cbd
supiccc <supiccc@gmail.com>
2017-03-12 19:53:34 +0800

    Add user microposts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,13 @@ class ApplicationController < ActionController::Base
   def hello
   	render html: "hello, world"
   end
+
+  private
+	  def logged_in_user
+	      unless logged_in?
+	        store_location
+	        flash[:danger] = "Please log in."
+	        redirect_to login_url
+	      end
+	  end
 end
